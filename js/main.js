e2bd5192f445e058acb7142026ab50765804cf05
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-03 15:45:46 +0100

    mostra bombe

---
+++ js/main.js
@@ -0,0 +1,88 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function renderBoard(container , cellNumber) {
+    for (let i = 1; i <= cellNumber ; i++) {
+        const boardCell = document.createElement("div");
+        boardCell.innerHTML = i;
+        boardCell.classList.add("cell");
+        boardCell.classList.add(`board-number-${cellNumber}`);
+        boardCell.addEventListener("click" , function () {
+            console.log( bomb)
+            console.log( this.innerHTML)
+            
+            if (bomb.includes(Number(this.innerHTML)) == true) {
+                for (let i = 0; i < bomb.length; i++) {
+                    
+                    document.querySelector('.board .cell:nth-child('+bomb[i]+')').classList.add('bomb')
+                }
+                
+                // tolgo eventlistener dal boardcell
+                
+            }else{
+                userClick += 1
+                this.classList.add("number-selected")
+                userPoint.innerHTML = userClick
+                console.log(goodCell)
+                if (userClick === goodCell){
+                    alert("HAi VINTO")
+                }
+            }
+
+            
+            
+                
+                
+        })
+        container.append(boardCell);
+    }
+}
+
+
+let bomb = []
+let goodCell = 0
+console.log(goodCell)
+const play = document.getElementById('play');
+const boardContainer = document.querySelector(".board")
+const userPoint = document.getElementById("point")
+let userClick = 0 
+
+
+play.addEventListener("click" , function() {
+    const levelVal = Number(document.getElementById('level').value);
+    goodCell = levelVal - 16
+    
+    userPoint.innerHTML = 0
+    console.log(levelVal);
+    while (bomb.length < 16 ) {
+        let generatedNumber = getRndInteger(1 , levelVal)
+        debugger
+        if (bomb.includes(generatedNumber) == false) {
+            bomb.push(generatedNumber)
+        }
+    }
+console.log(bomb)
+    boardContainer.innerHTML=""
+    userClick = 0
+    renderBoard(boardContainer , levelVal)
+})
+        
+    
+
+
+        
+                
+            
+
+
+
+    
+   
+    
+
+
+   
+    
+    
+    
